29f52756efd8af0bc9a5a6b72ae9b5a72463aba1
mog <mog@trbl.at>
2013-03-09 00:37:51 +0100

    first version, works only on bbs yet

---
+++ preflight.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name       preFlight
+// @version    0.0.1
+// @description  shows the preview of a thread response under the message field
+// @match      http://pouet.net/topic.php?which*
+// @copyright  2013+, mog@trbl.at
+// ==/UserScript==
+
+var textArea = document.querySelectorAll("textarea[name='message']")[0],
+    form = document.querySelectorAll("form[action='add.php']")[0],
+    previewFrame = document.createElement('iframe'),
+    firstKeypress = true,
+    cooldownTimer;
+
+previewFrame.setAttribute('name', 'previewFrame');
+previewFrame.setAttribute('scrolling', 'no');
+previewFrame.style.cssText = 'border:0;width:100%;';
+
+previewFrame.onload = resizePreview;
+
+textArea.onkeyup = startKBPreviewUpdateDDoSPreventionTimer;
+
+
+function startKBPreviewUpdateDDoSPreventionTimer(e) {
+  
+    clearInterval(cooldownTimer);
+	cooldownTimer = setTimeout(updatePreview, 500);
+}
+
+function updatePreview() {
+	
+    if(firstKeypress){
+        firstKeypress = false;
+        form.parentNode.insertBefore(previewFrame, form.nextSibling);
+    }
+    
+    clearInterval(cooldownTimer);
+    
+    var type = 'topic',
+		oldAction = form.action;
+    
+    previewFrame.setAttribute('src', 'preview_' + type + '.php');
+	form.action = 'preview_' + type + '.php';
+	form.target = 'previewFrame';
+  	form.submit();
+    
+    form.action = oldAction;
+    form.target = '_self';
+}
+
+function resizePreview() {
+	previewFrame.height = parseInt(previewFrame.contentWindow.document.body.scrollHeight, 10) + 'px';
+}
